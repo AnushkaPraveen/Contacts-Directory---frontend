8d0792c52eb3324295466205fdabc90c20f3fd59
AnushkaPraveen <anushkapraveen98@gmail.com>
2021-11-22 02:44:57 +0530

    setup route path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Dashboard from "./components/dashboard";
+import Home from './components/home';
+import Admin from './components/admin/admin';
+import Employee from './components/employee/employee';
+import Product from './components/product/product';
+import Supplier from './components/supplier/supplier';
+import Warranty from './components/warranty/warranty';
+import Service from './components/service/service';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+ 
+       <Router>
+         <Routes>
+       <Route path="/" element={<Home/>}/>
+       <Route path="/dashboard" element={<Dashboard/>}/>
+       <Route path="/admin" element={<Admin/>}/>
+       <Route path="/employee" element={<Employee/>}/>
+       <Route path="/product" element={<Product/>}/>
+       <Route path="/supplier" element={<Supplier/>}/>
+       <Route path="/service" element={<Service/>}/>
+       <Route path="/warranty" element={<Warranty/>}/>
+       </Routes>
+       </Router>
+ 
     </div>
   );
 }
---
+++ src/components/warranty/warranty.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Warranty extends Component{
+    render(){
+        return(
+            <div>
+               <h1>Welcome to Warranty</h1> 
+            </div>
+        )
+    }
+
+}
+export default Warranty;
